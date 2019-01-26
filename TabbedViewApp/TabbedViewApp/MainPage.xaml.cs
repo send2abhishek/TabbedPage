08ff45a87541b20db9cbea9fd0ffbf43615c5d00
Abhishek Aryan <send2abhishek@live.com>
2019-01-26 12:06:18 +0530

    Add project files.

---
+++ TabbedViewApp/TabbedViewApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbedViewApp
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var response = await DisplayActionSheet("Title", "Cancel", "Delete", "Copy", "Email");
+
+           await DisplayAlert("Response", response, "OKay");
+        }
+    }
+}
---
+++ TabbedViewApp/TabbedViewApp/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedViewApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           var response=  await DisplayAlert("Waring", "Are you Sure ?", "Yes", "No");
+
+            if (response)
+            {
+                await DisplayAlert("Done", "your Response has been Added", "Okay");
+            }
+        }
+    }
+}
